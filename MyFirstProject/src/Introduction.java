243974988e279267002f5c83c978fa5ecb534d87
AlecGuinan <42847351+AlecGuinan@users.noreply.github.com>
2021-03-24 10:20:28 -0700

    Add files via upload

---
+++ MyFirstProject/src/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args){
+        String name = "Alec Guinan";
+        String color = "Blue";
+        String lottery = "spend some on motorcycles and most into savings";
+        System.out.println("My name is " + name);
+        System.out.println("My favorite color is " + color);
+        System.out.println("If I won the lottery, I would " + lottery);
+    }
+}
